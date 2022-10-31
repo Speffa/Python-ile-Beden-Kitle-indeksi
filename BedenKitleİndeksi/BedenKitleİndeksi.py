1db1bedade19ebe27369c4686e27fa3130a56d5c
Speffa <atcekenemir@gmail.com>
2022-10-31 19:46:39 +0300

    Beden Kitle İndeksi
    Python ile kullanıcıdan alınan veriler ile beden kitle indeksi bulma programı

---
+++ BedenKitleİndeksi/BedenKitleİndeksi.py
@@ -0,0 +1,6 @@
+#Beden Kitle İndeksi
+boy = float(input("Lütfen Boyunuzu Giriniz(m) : "))
+kilo = int(input("Lütfen Kilonuzu Giriniz : "))
+BedenKitleİndeksi = kilo / (boy**2)
+i = BedenKitleİndeksi
+print(f"Beden Kitle İndeksiniz : {i}")
